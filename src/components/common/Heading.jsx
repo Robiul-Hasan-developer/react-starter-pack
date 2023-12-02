2bb934d765c68ef505beb0e05eca08f17b372b21
Robiul Hasan <robiulhasan9559@gmail.com>
2023-12-02 23:59:55 +0600

    React Starter Package

--- src/components/common/Heading.jsx
+++ src/components/common/Heading.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Heading = ({title, subtitle}) => {
+const Heading = ({title, subtitle, leftClass}) => {
     return (
         <>
-            <div className="section-heading">
+            <div className={`section-heading ${leftClass}`}>
                 <h2 className='section-heading__title'> {title} </h2>   
                 <p className='section-heading__desc'> {subtitle} </p>   
             </div>
